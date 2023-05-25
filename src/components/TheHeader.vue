33154eca5f7096b62d4cb6a52af50531f31b45d2
Tegemea Lema <lemajunior@hotmail.com>
2023-05-25 20:38:55 +0300

    navigation added

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { useTheme } from 'vuetify/lib/framework.mjs';
+import { watch, ref } from 'vue';
+
+const theme = useTheme();
+const themeIcon = ref('mdi-moon-waxing-crescent')
+
+const toggleTheme = () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+watch(
+    () => theme.global.name.value,
+    newTheme => themeIcon.value = newTheme === 'light' ? 'mdi-moon-waxing-crescent' : 'mdi-white-balance-sunny',
+    { deep: true }
+)
+</script>
+
+<template>
+    <VToolbar color="transparent">
+        <VToolbarTitle title="Arusha ICT Society">
+            <VImg cover width="200" src="@/assets/images/logo.svg"></VImg>
+        </VToolbarTitle>
+        <VBtn 
+            :ripple="false" 
+            cols="auto" 
+            @click.prevent="toggleTheme" 
+            class="mx-5" 
+            variant="plain"
+            title="Toggle Dark / Light modes">
+            <VIcon>{{ themeIcon }}</VIcon>
+        </VBtn>
+        <RouterLink to="/" class="text-decoration-none mx-3">
+            <VBtn elevation="4">
+                <span>Home</span>
+            </VBtn>
+        </RouterLink>
+        <RouterLink to="/register" class="text-decoration-none mx-3">
+            <VBtn elevation="4">
+                <span>Register</span>
+            </VBtn>
+        </RouterLink>
+        <RouterLink to="/members" class="text-decoration-none mx-3">
+            <VBtn elevation="4">
+                <span>Members</span>
+            </VBtn>
+        </RouterLink>
+    </VToolbar>
+</template>
