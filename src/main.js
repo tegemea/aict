654438c1f428aff1f0581a8e94abe125f9bed5a4
Tegemea Lema <lemajunior@hotmail.com>
2023-05-25 17:30:52 +0300

    vuetify configured & working fine

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components, directives
+})
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +16,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
