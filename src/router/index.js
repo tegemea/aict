976646fdf843a5d8bc6f3109091458e09a106aeb
Tegemea Lema <lemajunior@hotmail.com>
2023-05-26 18:17:04 +0300

    who we are page added

--- src/router/index.js
+++ src/router/index.js
@@ -4,22 +4,15 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    { path: '/register', name: 'Register', component: () => import('../views/RegisterView.vue') },
-    { path: '/members', name: 'Members', component: () => import('../views/MembersView.vue') },
+    { path: '/', name: 'Technology Enthusiasts', component: HomeView },
+    { path: '/who-we-are', name: 'Who We Are', component: () => import('../views/WhoWeAre.vue') },
+    { path: '/register', name: 'Register / Join us', component: () => import('../views/RegisterView.vue') },
+    { path: '/members', name: 'Our Members', component: () => import('../views/MembersView.vue') },
   ]
+})
+
+router.beforeEach((to) => {
+  document.title = `${to.name} - Arusha ICT Society`
 })
 
 export default router
---
+++ src/views/WhoWeAre.vue
@@ -0,0 +1,15 @@
+<template>
+    <VRow class="py-12 align-center">
+        <VCol cols="12" class="d-flex align-center">
+            <VDivider />
+                <h3 class="mx-4 text-green">WhoWeAre</h3>
+            <VDivider />
+        </VCol>
+        <VCol cols="12" md="6">
+            <VImg aspect-ratio src="src/assets/images/people.png" alt="ICT Enthusiasts" />
+        </VCol>
+        <VCol cols="12" md="6">
+            <p>Arusha ICT Society is a group of ICT enthusiasts currently in Arusha, with aim to join members for the betterment of their skills, living standards & the surrounding Tanzania community at large. It’s a non-governmental Organization, not affiliated to any Religious or Political entity</p>
+        </VCol>
+    </VRow>
+</template>
